ce53aade032f76a4336b23f3f7c527e2df287670
Muhammad-Maruff <muhammadmaruf2001@gmail.com>
2024-11-09 08:30:06 +0700

    change blade structure profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -4,8 +4,12 @@
 @section('title','Profile')
 
 @section('content')
+
 <div class="row profile-container">
     <div class="card profile-card">
+        <a href="pages/gallery.html" class="nav-link">
+            <i class="nav-icon fa fa-edit card-edit"></i>
+          </a>
         <div class="card-header">
             <div class="user-panel mt-3 mb-3 d-flex">
                 <div class="image">
@@ -13,30 +17,22 @@
                 </div>
             </div>
         </div>
-            <div class="card-content">
-                <section class="card-information">
-                    <div class="info-row">
-                        <div class="info-item">
-                            <label for="">Username</label>
-                            <p>{{ auth()->user()->username }}</p>
-                        </div>
-                        <div class="info-item">
-                            <label for="">Email</label>
-                            <p>{{ auth()->user()->email }}</p>
-                        </div>
-                    </div>
-                        <div class="info-row">
-                            <div class="info-item">
-                                <label for="">Phone Number</label>
-                                <p>{{ auth()->user()->phone }}</p>
-                            </div>
-                            <div class="info-item">
-                                <label for="">Address</label>
-                                <p>{{ auth()->user()->address }}</p>
-                            </div>
-                        </div>
-                </section>
-            </div>  
+
+        <div class="card-content">
+            <section class="content-one">
+                <label for="">Username</label>
+                <p>{{Auth()->user()->username}}</p>
+                <label for="">Email</label>
+                <p>{{Auth()->user()->email}}</p>
+            </section>
+            <section class="content-two">
+                <label for="">Phone</label>
+                <p>{{Auth()->user()->phone}}</p>
+                <label for="">Address</label>
+                <p>{{Auth()->user()->address}}</p>
+            </section>
+        </div>
+          
     </div>
 </div>
 
